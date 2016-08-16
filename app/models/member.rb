39eb586d1b34c19177351a6b98f3ba34f0869ef9
Unalterable <diamond.oliver@gmail.com>
2016-08-16 14:31:17 +0100

    csv_export

--- app/models/member.rb
+++ app/models/member.rb
@@ -5,9 +5,10 @@ class Member < ApplicationRecord
     CSV.foreach(file.path, headers: true) do |row|
 
       member_hash = row.to_hash
-      member = Member.where(id: member_hash["id"]).or(Member.where(email: member_hash["email"]))
+      member =  Member.where(id: member_hash["id"]).or(
+                Member.where(email: member_hash["email"]))
 
-      if member.count == 1
+      if member.count > 0
         member.first.update_attributes(member_hash)
       else
         Member.create!(member_hash)
